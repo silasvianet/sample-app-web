65c819d08c580890590f03413cd62fedf3465465
Silas Paixão Santos <46866601+silasvianet@users.noreply.github.com>
2020-05-21 18:54:51 -0300

    Ajuste

--- js/index.js
+++ js/index.js
@@ -11,14 +11,22 @@ console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
 
-const API_KEY = 'a8Vl-Sp-TxK9MvTsYiMe9A';
-
-/**
+//const API_KEY = 'a8Vl-Sp-TxK9MvTsYiMe9A';
+const API_KEY = 'uM_F3kaESMWW3EEbwGU4PA'
+  
+    /**
     * NEVER PUT YOUR ACTUAL API SECRET IN CLIENT SIDE CODE, THIS IS JUST FOR QUICK PROTOTYPING
     * The below generateSignature should be done server side as not to expose your api secret in public
     * You can find an eaxmple in here: https://marketplace.zoom.us/docs/sdk/native-sdks/Web-Client-SDK/tutorial/generate-signature
-    */
-const API_SECRET = 'erRkAbJXZlWDkKFtEP9umfAib3zUkYBf0k5Y';
+    **/
+	
+//const API_SECRET = 'erRkAbJXZlWDkKFtEP9umfAib3zUkYBf0k5Y';
+const API_SECRET = 'lV1D9buVms6BpUXUijp3YQE5GScAHDIusAoN';
+
+
+//chave: YK3IRTujQQuaOT4wIEI3yg
+//Segredo: SQ9xGS0zBQWksi9x0T8xxB5RoXbQrugdVgq0
+
 
 testTool = window.testTool;
 document.getElementById('display_name').value = "Local" + ZoomMtg.getJSSDKVersion()[0] + testTool.detectOS() + "#" + testTool.getBrowserInfo();
@@ -28,15 +36,42 @@ document.getElementById('meeting_pwd').value = testTool.getCookie('meeting_pwd')
 testTool.setCookie('meeting_lang', 'pt-PT');
 $.i18n.reload('pt-PT');
 
+
+//http://localhost:9999/index.html?IdAgenda=52653
+const querystring = require('querystring');
+const url		  = window.location.href;
+
+var getParams = function (url) {
+	var params = {};
+	var parser = document.createElement('a');
+	parser.href = url;
+	var query = parser.search.substring(1);
+	var vars = query.split('&');
+	for (var i = 0; i < vars.length; i++) {
+		var pair = vars[i].split('=');
+		params[pair[0]] = decodeURIComponent(pair[1]);
+	}
+	return params;
+};
+
+console.log(getParams(url).IdAgenda);
+
+
+//Localiza Od
+
+
+
 document.getElementById('clear_all').addEventListener('click', (e) => {
     testTool.deleteAllCookies();
     document.getElementById('display_name').value = '';
     document.getElementById('meeting_number').value = '';
     document.getElementById('meeting_pwd').value = '';
 });
+
+
 document.getElementById('join_meeting').addEventListener('click', (e) => {
     e.preventDefault();
-
+	
     const meetConfig = {
         apiKey: API_KEY,
         apiSecret: API_SECRET,
@@ -44,7 +79,7 @@ document.getElementById('join_meeting').addEventListener('click', (e) => {
         userName: document.getElementById('display_name').value,
         passWord: document.getElementById('meeting_pwd').value,
         leaveUrl: 'https://zoom.us',
-        role: parseInt(0, 10)
+        role: parseInt(1, 10)
     };
     testTool.setCookie('meeting_number', meetConfig.meetingNumber);
     testTool.setCookie('meeting_pwd', meetConfig.passWord);
